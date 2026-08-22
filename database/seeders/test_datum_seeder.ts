27a0599365f324318be7c0d17dd76be0f58a1c35
Emiliano Aguilar <133832012+Sir-Fehane@users.noreply.github.com>
2026-08-21 19:04:03 -0600

    feat(seeders): agregar usuarios base para prod

--- database/seeders/test_datum_seeder.ts
+++ database/seeders/test_datum_seeder.ts
@@ -2,37 +2,45 @@ import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import Usuario from '#modules/identidad/models/usuario'
 import { randomUUID } from 'node:crypto'
 import hash from '@adonisjs/core/services/hash'
-import app from '@adonisjs/core/services/app'
-import { InvitacionService } from '#modules/identidad/services/invitacion_service'
 
 export default class extends BaseSeeder {
   async run() {
-    console.log('Creando usuario admin inicial...')
-    const admin = await Usuario.create({
-      uuidUsuario: randomUUID(),
-      idRol: 1, // 1 es admin
-      nombre: 'Fehane',
-      apellidoPaterno: 'Prueba',
-      correo: 'fehane91@gmail.com',
-      passwordHash: await hash.make('Admin1234'),
-      biometriaHabilitada: false,
-      activo: true,
-    })
+    console.log('Creando usuarios iniciales...')
+    const passwordHash = await hash.make('17Nov2002!')
 
-    console.log('Generando invitación para mesero...')
-    const invitaciones = await app.container.make(InvitacionService)
-    const inv = await invitaciones.invitar({
-      idEmisor: admin.id,
-      idRolDestino: 3, // mesero
-      nombre: 'Mesero',
-      apellidoPaterno: 'SGEB',
-      correo: 'emilianoa.aguilar17@gmail.com'
-    })
+    await Usuario.createMany([
+      {
+        uuidUsuario: randomUUID(),
+        idRol: 1, // admin
+        nombre: 'Admin',
+        apellidoPaterno: 'Fehane',
+        correo: 'fehane91@gmail.com',
+        passwordHash,
+        biometriaHabilitada: false,
+        activo: true,
+      },
+      {
+        uuidUsuario: randomUUID(),
+        idRol: 2, // capitan
+        nombre: 'Capitan',
+        apellidoPaterno: 'Emijoker',
+        correo: 'emijoker17@gmail.com',
+        passwordHash,
+        biometriaHabilitada: false,
+        activo: true,
+      },
+      {
+        uuidUsuario: randomUUID(),
+        idRol: 3, // mesero
+        nombre: 'Mesero',
+        apellidoPaterno: 'Emiliano',
+        correo: 'emilianoa.aguilar17@gmail.com',
+        passwordHash,
+        biometriaHabilitada: false,
+        activo: true,
+      },
+    ])
 
-    console.log('\n==========================================================')
-    console.log('🎉 ¡Invitación lista!')
-    console.log('👉 Entra a esta URL en tu navegador (asegúrate de tener el SSO corriendo en el 4321):')
-    console.log(`http://localhost:4321/registro?token=${inv.token}`)
-    console.log('==========================================================\n')
+    console.log('✅ 3 usuarios creados exitosamente.')
   }
 }
